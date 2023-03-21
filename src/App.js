91c7206695b43d2f9958fbaf3013ffd3b5f032ad
Arturo Vargas Romero <arturo.vargas@laika.pet>
2023-03-20 20:21:29 -0500

    Estados simples: react component vs usestate

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { UseState } from "./UseState";
+import { ClassState } from "./ClassState";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <UseState name="UseState" />
+      <ClassState name="ClassState" />
+    </div>
+  );
+}
+
+export default App;
